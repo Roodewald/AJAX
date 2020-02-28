7e3940dcf0a38c2ca6525451f9000603b6b480d2
Roodewald <55383286+Roodewald@users.noreply.github.com>
2020-02-29 01:12:26 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ function dd (){
         url: "data.php",
         type: 'POST',
         success: function(e){
-          $("main").empty();
+          $(".eshop").empty();
 
           e = jQuery.parseJSON(e)//Парсируем JSON
-            for(i=0; i<e.length; i++){//Вывести все массивы
-              var html = '<div>'+e[i]+'</div>';//Создаем код HTML
-            $("main").append(html)//Добавить в мейн код HTML снизу
+            for(var i=0; i<e.length; i++){//Вывести все массивы
+              var html = '<div class="eshopItem"><p class="service">'+ e[i][0] +'</p><p class="service-dis">'+ e[i][1] +'</p><p class="price">'+ e[i][2] +'</p></div>';
+            $(".eshop").append(html)//Добавить в мейн код HTML снизу
             };
         }
      });          
+}
+
+function google_analytics(){
+          $(".eshop").empty();
 }
