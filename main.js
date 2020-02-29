b612cbec7a8643468e899bf8987ffa19cac36ca5
Roodewald <55383286+Roodewald@users.noreply.github.com>
2020-02-29 13:05:04 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -10,10 +10,18 @@ function dd (){
               var html = '<div class="eshopItem"><p class="service">'+ e[i][0] +'</p><p class="service-dis">'+ e[i][1] +'</p><p class="price">'+ e[i][2] +'</p></div>';
             $(".eshop").append(html)//Добавить в мейн код HTML снизу
             };
+          var htmlButton = '<button class="search1" type="submit" name="form_bth" onclick="clear_items()">Скрыть тайну</button>';
+          $(".buttonplace").append(htmlButton);
         }
      });          
 }
 
-function google_analytics(){
-          $(".eshop").empty();
+var createItems = '<div class="eshopItem"><p class="service">Получив эту информацию вы соглошаетесь с неразглашением комерческой тайны  *УК РФ Статья 183*</p><button class="search1" type="submit" name="form_bth" onclick="dd()">Получить информацию об услугах</button></div> '
+
+
+function clear_items(){
+  $(".eshop").empty();
+  $(".buttonplace").empty();
+
+  $(".eshop").append(createItems);
 }
